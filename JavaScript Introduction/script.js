af04cbe511330ee88b3f94b1bea8f1c607d6289f
Samarpan KC <110466655+SamarpanKc@users.noreply.github.com>
2024-02-03 17:46:11 +0545

    Add files via upload

---
+++ JavaScript Introduction/script.js
@@ -0,0 +1,14 @@
+alert("Feb-2, 2024 I just begin with Java Script and i'm gonna crack it!")
+console.log("Code is dukuring...")
+console.log("Code is very fast dukuring...")
+console.log("Code is beautifully dukuring...")
+
+var a = prompt("Enter your number")
+console.log("Your Number is: " +a)
+var isTrue = confirm("Are you sure want to chodera jana malai ?")
+if(isTrue){
+    console.log("OK Good Bye!")
+}
+else{
+    console.log("Thank You for staying!")
+}
