0bdb471a6a344d9db5475b96eb06def454f67e2e
Samarpan KC <110466655+SamarpanKc@users.noreply.github.com>
2024-03-04 21:29:41 +0545

    Add files via upload

---
+++ JavaScript String/script.js
@@ -0,0 +1,26 @@
+let a = "Samarpan KC";
+console.log(a);
+console.log(typeof a);
+console.log(a.toUpperCase());
+console.log(a.slice(1, 6));
+console.log(a.slice(1));
+console.log(a.replace("Sam", 54));
+
+
+// Accessing Character
+console.log(a[0]);
+console.log(a[1]);
+
+// length of string
+console.log(a.length);
+
+// 
+let hisName = "Nitesh";
+let hisfriend = "Parsant";
+console.log("His name is "+ hisName +" and his friend name is "+hisfriend);
+// Template Literals
+console.log(`His name is ${hisName} and his friend name is ${hisfriend}`);
+
+// concatinate strings
+console.log(hisName.concat(hisfriend, "Sumit"));
+
